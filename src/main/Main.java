28020b81fadb0b3290caecfd0a7c7cc9b6fb22ed
hirof <hirofool502@gmail.com>
2023-10-17 20:23:47 +0300

    two

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,14 +5,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1() {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         System.out.println(onlyOddNums(nums));
+    }
+    public static void task2() {
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(onlyEvenNums(nums));
     }
 
     private static String onlyOddNums(List<Integer> nums) {
         List<Integer> integers = new ArrayList<>();
         for (Integer num : nums) {
             if (!(num % 2 == 0)) {
+                integers.add(num);
+            }
+        }
+        return integers.toString();
+    }
+
+    private static String onlyEvenNums(List<Integer> nums) {
+        List<Integer> integers = new ArrayList<>();
+        for (Integer num : nums) {
+            if (num % 2 == 0) {
                 integers.add(num);
             }
         }
