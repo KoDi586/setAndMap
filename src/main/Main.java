5fba6e00142e9b846f3a27d2968c6cc846b99bea
hirof <hirofool502@gmail.com>
2023-10-17 20:19:56 +0300

    one

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,21 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(onlyOddNums(nums));
+    }
+
+    private static String onlyOddNums(List<Integer> nums) {
+        List<Integer> integers = new ArrayList<>();
+        for (Integer num : nums) {
+            if (!(num % 2 == 0)) {
+                integers.add(num);
+            }
+        }
+        return integers.toString();
     }
 }
